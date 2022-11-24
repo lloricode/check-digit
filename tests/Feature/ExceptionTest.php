337104c1d215c70fe75f21c926916d9553913aba
Lloric Mayuga Garcia <lloricode@gmail.com>
2022-11-24 08:37:25 +0800

    Use pest syntax
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- tests/Feature/ExceptionTest.php
+++ tests/Feature/ExceptionTest.php
@@ -6,15 +6,17 @@ use Lloricode\CheckDigit\Exceptions\ValidationException;
 use Lloricode\CheckDigit\Generator;
 
 it('will throw exception when invalid format', function () {
-    $this->expectException(ValidationException::class);
-    $this->expectErrorMessage('Invalid format `xxxx`.');
-
     (new Generator(1, 'xxxx'));
-});
+})
+->throws(
+    ValidationException::class,
+    'Invalid format `xxxx`.'
+);
 
 it('will throw exception when invalid length', function () {
-    $this->expectException(ValidationException::class);
-    $this->expectErrorMessage('Invalid length of `1` for format `GTIN-13`, with value `1`');
-
     (new Generator(1, Generator::GTIN_13));
-});
+})
+    ->throws(
+        ValidationException::class,
+        'Invalid length of `1` for format `GTIN-13`, with value `1`'
+    );
--- tests/Feature/GenerateTest.php
+++ tests/Feature/GenerateTest.php
@@ -4,37 +4,39 @@ declare(strict_types=1);
 
 use Lloricode\CheckDigit\Generator;
 
+use function PHPUnit\Framework\assertSame;
+
 it('generator', function () {
     # base in https://www.gs1.org/services/check-digit-calculator
 
-    $this->assertEquals(2, (new Generator(9638527, Generator::GTIN_8))->getCheckDigit());
-    $this->assertEquals(4, (new Generator(3216549, Generator::GTIN_8))->getCheckDigit());
+    assertSame(2, (new Generator(9638527, Generator::GTIN_8))->getCheckDigit());
+    assertSame(4, (new Generator(3216549, Generator::GTIN_8))->getCheckDigit());
 
-    $this->assertEquals(2, (new Generator(91739456321, Generator::GTIN_12))->getCheckDigit());
-    $this->assertEquals(8, (new Generator(74185245963, Generator::GTIN_12))->getCheckDigit());
+    assertSame(2, (new Generator(91739456321, Generator::GTIN_12))->getCheckDigit());
+    assertSame(8, (new Generator(74185245963, Generator::GTIN_12))->getCheckDigit());
 
-    $this->assertEquals(3, (new Generator(629104150021, Generator::GTIN_13))->getCheckDigit());
-    $this->assertEquals(6, (new Generator(123456789876, Generator::GTIN_13))->getCheckDigit());
+    assertSame(3, (new Generator(629104150021, Generator::GTIN_13))->getCheckDigit());
+    assertSame(6, (new Generator(123456789876, Generator::GTIN_13))->getCheckDigit());
 
-    $this->assertEquals(6, (new Generator(7539514528423, Generator::GTIN_14))->getCheckDigit());
-    $this->assertEquals(5, (new Generator(8563251459762, Generator::GTIN_14))->getCheckDigit());
+    assertSame(6, (new Generator(7539514528423, Generator::GTIN_14))->getCheckDigit());
+    assertSame(5, (new Generator(8563251459762, Generator::GTIN_14))->getCheckDigit());
 
-    $this->assertEquals(0, (new Generator(7896541230123456, Generator::GSIN))->getCheckDigit());
-    $this->assertEquals(3, (new Generator(7658485040650456, Generator::GSIN))->getCheckDigit());
+    assertSame(0, (new Generator(7896541230123456, Generator::GSIN))->getCheckDigit());
+    assertSame(3, (new Generator(7658485040650456, Generator::GSIN))->getCheckDigit());
 
-    $this->assertEquals(6, (new Generator(95135623050123698, Generator::SSCC))->getCheckDigit());
-    $this->assertEquals(7, (new Generator(87643802105978513, Generator::SSCC))->getCheckDigit());
+    assertSame(6, (new Generator(95135623050123698, Generator::SSCC))->getCheckDigit());
+    assertSame(7, (new Generator(87643802105978513, Generator::SSCC))->getCheckDigit());
 });
 
 it('generate with string and start with zero', function () {
-    $this->assertEquals(7, (new Generator('0012345', Generator::GTIN_8))->getCheckDigit());
-    $this->assertEquals(5, (new Generator('00123456789', Generator::GTIN_12))->getCheckDigit());
-    $this->assertEquals(5, (new Generator('001234567890', Generator::GTIN_13))->getCheckDigit());
-    $this->assertEquals(2, (new Generator('0012345678901', Generator::GTIN_14))->getCheckDigit());
-    $this->assertEquals(3, (new Generator('0012345678901234', Generator::GSIN))->getCheckDigit());
-    $this->assertEquals(2, (new Generator('00123456789012345', Generator::SSCC))->getCheckDigit());
+    assertSame(7, (new Generator('0012345', Generator::GTIN_8))->getCheckDigit());
+    assertSame(5, (new Generator('00123456789', Generator::GTIN_12))->getCheckDigit());
+    assertSame(5, (new Generator('001234567890', Generator::GTIN_13))->getCheckDigit());
+    assertSame(2, (new Generator('0012345678901', Generator::GTIN_14))->getCheckDigit());
+    assertSame(3, (new Generator('0012345678901234', Generator::GSIN))->getCheckDigit());
+    assertSame(2, (new Generator('00123456789012345', Generator::SSCC))->getCheckDigit());
 });
 
 it('get generated value', function () {
-    $this->assertEquals(876438021059785137, (new Generator(87643802105978513, Generator::SSCC))->getValue());
+    assertSame(876438021059785137, (new Generator(87643802105978513, Generator::SSCC))->getValue());
 });
